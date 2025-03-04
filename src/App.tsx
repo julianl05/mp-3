53527afc08fb67938caa78919c79fe9039b37c23
Julian Lim Jun Ren <limjunre@bu.edu>
2025-03-04 17:37:04 -0500

    routing implementaiton in App done

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,50 @@ import {createBrowserRouter, RouterProvider, Routes, Route} from "react-router"
 import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
-
+import Home from "./components/Home"
+import Education from "./components/Education"
+import Experiences from "./components/Experiences"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
+import Activities from "./components/Activities"
 
 const StyledPageWraper = styled.div`
   width: 80vw; 
   margin: 0 auto;
 `
+
+const StyledContentContainer = styled.div`
+
+`
 function Root(){
   return(
-    <Routes>
-      <Route path={`/`} element={<Home />} />
-      <Route path={`/education`} element={<Education />} />
-      <Route path={`/experiences`} element={<Experiences />} />
-      <Route path={`/projects`} element={<Projects />} />
-      <Route path={`/skills`} element={<Skills />} />
-      <Route path={`/activities`} element={<Activities />} />
-    </Routes>
+    <StyledPageWraper>
+      <Header/>
+      <StyledContentContainer>
+        <Navbar/>
+        <Routes>
+          <Route path={`/`} element={<Home />} />
+          <Route path={`/education`} element={<Education />} />
+          <Route path={`/experiences`} element={<Experiences />} />
+          <Route path={`/projects`} element={<Projects />} />
+          <Route path={`/skills`} element={<Skills />} />
+          <Route path={`/activities`} element={<Activities />} />
+        </Routes>
+      </StyledContentContainer>
+      <Footer/>
+    </StyledPageWraper>
   );
 }
+
+const router = createBrowserRouter(
+  [{path:"*", Component: Root},]
+);
 
 function App() {
 
   return (
     <>
+      <RouterProvider router={router}/>
     </>
   )
 }
