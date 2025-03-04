31dd50fc601ae69345cb427716ad71f8877feb59
Julian Lim Jun Ren <limjunre@bu.edu>
2025-03-04 17:12:15 -0500

    added footer header navbar components and installed styled-components

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Julian Lim Jun Ren</h1>
+            <p>Online Resume</p>
+        </header>
+    );
+}
